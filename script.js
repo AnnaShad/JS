c27ee91f23db19c5868eacdab730c08655b81d0a
Anna <ane4kaSonce@gmail.com>
2021-11-20 17:07:39 +0300

    Lessons 5

--- script.js
+++ script.js
@@ -1,124 +1,173 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-class ProductList{
-    constructor(container='.products'){
-        this.container = container;
-        this.goods = [];
-        this.sumPrice();
-        this._getProducts()
+const app = new Vue ({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        filtered: [],
+        imgCatalog: 'https://picsum.photos/200',
+        userSearch: '',
+        show: false
+    },
+    methods: {
+        filter(){
+            const regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(el =>
+                regexp.test(el.product_name));
+                
+        },
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+            },
+        addProduct(product){
+            console.log(product.id_product);
+        }
+    },
+        mounted(){
+            this.getJson(`${API + this.catalogUrl}`)
                 .then(data => {
-                    this.goods = data;
-                    this.render()
-                }) 
+                    for(let el of data){
+                        this.products.push(el);
+                        
+                    }
+                });
+            this.getJson(`getProducts1.json`)
+                .then(data => {
+                    for (let el of data){
+                        this.products.push(el);
+                        
+                    }
+                });
+        }
+
+})
+
+
+// class ProductList{
+//     constructor(container='.products'){
+//         this.container = container;
+//         this.goods = [];
+//         this.sumPrice();
+//         this._getProducts()
+//                 .then(data => {
+//                     this.goods = data;
+//                     this.render()
+//                 }) 
         
-    }
+//     }
  
 
-    _getProducts(){
+//     _getProducts(){
 
-        return fetch(`${API}/catalogData.json`)
-            .then(result => result.json())
-            .catch(error => {
-                console.log(error);
-            }); 
-    }
-    render(){
-        const block = document.querySelector(this.container);
-        for(let product of this.goods){
-            const item = new ProductItem(product);
-            block.insertAdjacentHTML("beforeend", item.render());
-        } 
-    }
-    sumPrice() {
-        let  sum = 0
-        this.goods.forEach(good => {  
-            let goodSum = good.price;
-            sum += goodSum;
-        }); 
-        console.log(`Сумма всех товаров ${sum}`); 
-    }
-}
+//         return fetch(`${API}/catalogData.json`)
+//             .then(result => result.json())
+//             .catch(error => {
+//                 console.log(error);
+//             }); 
+//     }
+//     render(){
+//         const block = document.querySelector(this.container);
+//         for(let product of this.goods){
+//             const item = new ProductItem(product);
+//             block.insertAdjacentHTML("beforeend", item.render());
+//         } 
+//     }
+//     sumPrice() {
+//         let  sum = 0
+//         this.goods.forEach(good => {  
+//             let goodSum = good.price;
+//             sum += goodSum;
+//         }); 
+//         console.log(`Сумма всех товаров ${sum}`); 
+//     }
+// }
 
 
-class ProductItem{
-    constructor(product, img = "https://picsum.photos/200"){
-        this.product_name = product.product_name;
-        this.id = product.id;
-        this.price = product.price;
-        this.img = img;
+// class ProductItem{
+//     constructor(product, img = "https://picsum.photos/200"){
+//         this.product_name = product.product_name;
+//         this.id = product.id;
+//         this.price = product.price;
+//         this.img = img;
         
-    }
-    render(){
-        return `<div class="goods-item">
-            <img src = ${this.img} alt="">
-            <h3>${this.product_name}</h3>
-            <p>${this.price}$</p>
-            <button class = "buy-btn">Купить</button>
-        </div>`;
-    }
-}
+//     }
+//     render(){
+//         return `<div class="goods-item">
+//             <img src = ${this.img} alt="">
+//             <h3>${this.product_name}</h3>
+//             <p>${this.price}$</p>
+//             <button class = "buy-btn">Купить</button>
+//         </div>`;
+//     }
+// }
 
 
-let list = new ProductList();
+// let list = new ProductList();
 
 
-class BasketList {
-    constructor(cont='.backet') {
-        this.cont = cont;
-        this.basket = [];
-        this._basketProduct()
-            .then(data => {
-                this.basket = data.contents;
-                this.rend()
-            })
+// class BasketList {
+//     constructor(cont='.backet') {
+//         this.cont = cont;
+//         this.basket = [];
+//         this._basketProduct()
+//             .then(data => {
+//                 this.basket = data.contents;
+//                 this.rend()
+//             })
 
-    }
+//     }
 
 
-    _basketProduct() {
+//     _basketProduct() {
 
-        return fetch(`${API}/getBasket.json`)
-            .then(res => res.json())
-            .catch(error => {
-                console.log(error);
-            });
-    }
-    rend() {
-        const bloc = document.querySelector(this.cont);
-        for (let prod of this.basket) {
-            const item = new BasketItem(prod);
-            bloc.insertAdjacentHTML("beforeend", item.rend());
-        }
-    }
+//         return fetch(`${API}/getBasket.json`)
+//             .then(res => res.json())
+//             .catch(error => {
+//                 console.log(error);
+//             });
+//     }
+//     rend() {
+//         const bloc = document.querySelector(this.cont);
+//         for (let prod of this.basket) {
+//             const item = new BasketItem(prod);
+//             bloc.insertAdjacentHTML("beforeend", item.rend());
+//         }
+//     }
     
-}
+// }
 
 
-class BasketItem {
-    constructor(prod, img = "https://picsum.photos/50") {
-        this.product_name = prod.product_name;
-        this.id_product = prod.id_product;
-        this.price = prod.price;
-        this.quantity = prod.quantity;
-        this.img = img;
+// class BasketItem {
+//     constructor(prod, img = "https://picsum.photos/50") {
+//         this.product_name = prod.product_name;
+//         this.id_product = prod.id_product;
+//         this.price = prod.price;
+//         this.quantity = prod.quantity;
+//         this.img = img;
 
-    }
-    rend() {
-        return `<div class="backet-one">
-                        <div class="backet-two bk">
-                            <div class="backet-img"><img src= ${this.img} alt=""></div>
-                            <div class="backet-four">
-                                <h3 class="name">${this.product_name}</h3>
-                                <p class="quantity">Quantity <span>${this.quantity}</span></p>
-                                <p class="each">2545</p>
-                            </div>
-                        </div>
-                        <div class="backet-three">
-                            <h3 class="backet-price">${this.price}</h3>
-                        </div>
-                 </div>`;
-    }
-}
+//     }
+//     rend() {
+//         return `<div class="backet-one">
+//                         <div class="backet-two bk">
+//                             <div class="backet-img"><img src= ${this.img} alt=""></div>
+//                             <div class="backet-four">
+//                                 <h3 class="name">${this.product_name}</h3>
+//                                 <p class="quantity">Quantity <span>${this.quantity}</span></p>
+//                                 <p class="each">2545</p>
+//                             </div>
+//                         </div>
+//                         <div class="backet-three">
+//                             <h3 class="backet-price">${this.price}</h3>
+//                             <span class="close"></span>
+//                         </div>
+//                  </div>`;
+//     }
+// }
 
 
-let list2 = new BasketList();
+// let list2 = new BasketList();
